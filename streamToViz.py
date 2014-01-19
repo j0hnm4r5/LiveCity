b28891b145da8d8dfa34eaad48848905b21b3482
John Mars <jmars764@gmail.com>
2014-01-19 02:38:53 -0500

    streamToViz works very well

--- streamToViz.py
+++ streamToViz.py
@@ -6,8 +6,16 @@ from twython import TwythonStreamer
 from twitterCredentials import *
 
 from pyprocessing import *
+import pprint
 
+# LOCATIONS
+world = "-180,-90,180,90"
+europe = "-13,34,41,60"
+usa = "-127,25,-64,50"
+manhattan = "-74,40.7,-73.9,40.9"
 
+loc = world
+geoBounds = loc.split(",")
 
 class StreamThread(Thread):
 	""" Thread that initializes and runs Twython Stream """
@@ -18,8 +26,11 @@ class StreamThread(Thread):
 
 	def run(self):
 		self.stream = LiveStream(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET, self.stream_queue)
-		print "STREAM INITIALIZED"
-		self.stream.statuses.filter(locations="-180,-90,180,90")
+		print "\nSTREAM INITIALIZED\n"
+
+		
+		self.stream.statuses.filter(locations=loc)
+		
 		# self.stream.statuses.sample()
 
 
@@ -33,16 +44,13 @@ class LiveStream(TwythonStreamer):
 
 	def on_success(self, data):
 		# PUT COORDS IN QUEUE
-		if 'coordinates' in data:
-			if data['coordinates'] != None:
-				coordinates = data['coordinates']['coordinates']
-				place = None
-				if 'place' in data:
-					if data['place'] != None:
-						if 'name' in data['place']:
-							if data['place']['name'] != None:
-								place = data['place']['name']
-				self.stream_queue.put((coordinates, place))
+		try:
+			coordinates = data['coordinates']['coordinates']
+			self.stream_queue.put(coordinates)
+		except TypeError:
+			pass
+		except KeyError:
+			pass
 
 	def on_error(self, status_code, data):
 		print('Error: {0}'.format(status_code))
@@ -54,27 +62,30 @@ streaming.start()
 def setup():
 	global scale_factor
 	scale_factor = 2
-	size(360 * scale_factor, 180 * scale_factor)
+
+	genWidth = int(abs(float(geoBounds[0]) - float(geoBounds[2])) * scale_factor)
+	genHeight = int(abs(float(geoBounds[1]) - float(geoBounds[3])) * scale_factor)
+	print genWidth, genHeight
+	size(genWidth, genHeight, caption=loc)
 	background(0)
 	noStroke()
-	rectMode(CENTER)
 
 def draw():
 	global stream_queue
 
-	# ACCOUNT FOR NEGATIVE LAT/LONG
-	translate(180 * scale_factor, 90 * scale_factor)
-
 	# DIM OLD DOTS
-	fill(0, 10)
+	fill(255, 0, 0, 5)
 	rect(0, 0, width, height)
 	
 	# GET COORDS
-	x = stream_queue.get()[0][0] * scale_factor
-	y = -stream_queue.get()[0][1] * scale_factor
-	place = stream_queue.get()[1]
+	x = stream_queue.get()[0]
+	y = stream_queue.get()[1]
+	print x, y
+
+	x = map(x, float(geoBounds[0]), float(geoBounds[2]), 0, width)
+	y = height - map(y, float(geoBounds[1]), float(geoBounds[3]), 0, height)
+	print x, y
 	stream_queue.task_done()
-	print -y, x, place.encode('utf-8')
 
 	#CREATE BLUR/GLOW
 	for i, v in enumerate(range(90, 0, -15)):
